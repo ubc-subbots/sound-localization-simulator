30266d13c0d6bd981296e66b34310b78b38e041f
dvirhilu <dvirhilu16@gmail.com>
2020-07-20 00:05:33 -0700

    finished gradient descent, working on nelder mead

--- src/TDOA_position_calculation/optimization.py
+++ src/TDOA_position_calculation/optimization.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
+from statistics import stdev
 
 # TODO: documentation (RIP)
 def gradient_descent(grad_func, starting_params, step_size=0.5, termination_ROC=0.001, max_iter=1e5):
@@ -13,4 +14,43 @@ def gradient_descent(grad_func, starting_params, step_size=0.5, termination_ROC=
         params = params - step_size*gradient
         gradient = grad_func(params)
         grad_mag = np.linalg.norm(gradient)
-    
+        num_iter += 1
+
+        if (num_iter == max_iter):
+            print("WARNING! maximum iteration number reached")
+            break
+
+    print("Gradient Descent with step-size %.1f finished after %0d iterations" %(step_size, num_iter))
+    return params
+
+def nelder_mead(func, starting_params, alpha=1, gamma=2, rho=0.5, sigma=0.5, 
+                    termination_stdev=0.01, max_iter=1e5, init_simplex_scale=0.5):
+    params = starting_params
+    simplex = [params]
+    for i in range(len(params)):
+        next_vertex_adder = np.zeros(params.shape)
+        next_vertex_adder[i] = params[i]
+        simplex.append(params+next_vertex_adder)
+    num_iter = 0
+
+    simplex_stdev = calculate_simplex_stdev(func, simplex)
+
+    while(simplex_stdev > termination_stdev):
+        simplex = calculate_next_simplex(func, simplex, alpha, gamma, rho, sigma)
+        simplex_stdev = calculate_simplex_stdev(func, simplex)
+        num_iter += 1
+
+        if (num_iter == max_iter):
+            print("WARNING! maximum iteration number reached")
+            break
+
+    simplex_min_index = min([func(vertex) for vertex in simplex])
+    print("Nelder Mead finished after %0d iterations" %(num_iter))
+
+    return simplex[simplex_min_index]
+
+def calculate_next_simplex(func, simplex, alpha, gamma, rho, sigma):
+    return simplex
+
+def calculate_simplex_stdev(func, simplex):
+    return stdev([func(vertex) for vertex in simplex])
---
+++ test/TDOA_position_calculation_test/optimization_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from TDOA_position_calculation import optimization
+from scipy import optimize
+import numpy as np
+
+def quartic(x):
+    return x**4
+
+def quartic_grad(x):
+    return 4*x**3
+
+def paraboloid(params):
+    return params[0]**2 + params[1]**2
+
+def paraboloid_grad(params):
+    return np.array([2*params[0], 2*params[1]])
+
+starting_params = np.array([20])
+
+# step size too big - diverges
+# optimized_params = optimization.gradient_descent(quartic_grad, starting_params)
+# print("Gradient Descent optimized the parameters to be:" + str(optimized_params))
+
+# optimized_params = optimization.gradient_descent(quartic_grad, starting_params, step_size=1e-3)
+# print("Gradient Descent optimized the parameters to be:" + str(optimized_params))
+
+starting_params = np.array([10, 12.3])
+
+optimized_params = optimization.gradient_descent(paraboloid_grad, starting_params, step_size=0.7)
+print("Gradient Descent optimized the parameters to be:" + str(optimized_params))
