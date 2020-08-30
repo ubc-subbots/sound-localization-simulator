643e416c55f01bbcfa6ce2455042c750804770d0
dvirhilu <dvirhilu16@gmail.com>
2020-08-30 03:16:59 -0700

    untracked doxygen files

---
+++ docs/doxygen/html/search/all_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['generate_5fhydrophone_5fresponse_0',['generate_hydrophone_response',['../namespacesrc_1_1sim__utils_1_1input__generation.html#a60208dee5f3cd04654fe6f83eec2b865',1,'src::sim_utils::input_generation']]],
+  ['generate_5fsine_5fwave_1',['generate_sine_wave',['../namespacesrc_1_1sim__utils_1_1input__generation.html#af8d6bf8361d168435b56819aedb052c3',1,'src::sim_utils::input_generation']]],
+  ['generate_5fsquare_5fwave_2',['generate_square_wave',['../namespacesrc_1_1sim__utils_1_1input__generation.html#a349a3bba785d290310adbe37fa14ea32',1,'src::sim_utils::input_generation']]],
+  ['get_5fphase_3',['get_phase',['../namespacesrc_1_1components_1_1time__difference__calculations_1_1phase__analysis.html#a27dd235a7a1ad7e4e2aa289bccf89a75',1,'src::components::time_difference_calculations::phase_analysis']]],
+  ['gradient_5fdescent_4',['gradient_descent',['../namespacesrc_1_1components_1_1_t_d_o_a__position__calculation_1_1optimization.html#ace2b9f615ede4baafb2c21d6a5f8daad',1,'src::components::TDOA_position_calculation::optimization']]]
+];
---
+++ docs/doxygen/html/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['nelder_5fmead_45',['nelder_mead',['../namespacesrc_1_1components_1_1_t_d_o_a__position__calculation_1_1optimization.html#a0e1765e05b4039afaf1037cb8c74eaa3',1,'src::components::TDOA_position_calculation::optimization']]],
+  ['newton_5fgauss_46',['newton_gauss',['../namespacesrc_1_1components_1_1_t_d_o_a__position__calculation_1_1optimization.html#ae36773d43f890858aaac04e92227e3ff',1,'src::components::TDOA_position_calculation::optimization']]]
+];
