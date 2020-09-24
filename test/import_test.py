a48c170162e93ffc52b52366eafc422fb2c46eb8
dvirhilu <dvirhilu16@gmail.com>
2020-09-23 23:14:13 -0700

    figured out simulator-env for now

--- test/import_test.py
+++ test/import_test.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 print("******* Python Import Test *******")
 from input_generation import input_generation
 print("input_generation/input_generation.py imported successfully!")
@@ -9,3 +7,5 @@ from TDOA_position_calculation import optimization, nonlinear_least_squares, ana
 print("TDOA_position_calculation/optimization.py imported successfully!")
 print("TDOA_position_calculation/nonlinear_least_squares.py imported successfully!")
 print("TDOA_position_calculation/analytical_solution.py imported successfully!")
+
+import config
