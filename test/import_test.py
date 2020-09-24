0f9089fe9c6569a61a2720f2cb628c621126540f
dvirhilu <31719321+dvirhilu@users.noreply.github.com>
2020-09-24 00:17:47 -0700

    Simulator env (#3)
    * started experimenting with anaconda environments. Created sim-env directory
    
    * untracked doxygen files
    
    * started experimenting with anaconda environments. Created sim-env directory
    
    * untracked doxygen files
    
    * started experimenting with anaconda environments. Created sim-env directory
    
    * untracked doxygen files
    
    * started experimenting with anaconda environments. Created sim-env directory
    
    * untracked doxygen files
    
    * started experimenting with anaconda environments. Created sim-env directory
    
    * untracked doxygen files
    
    * started experimenting with anaconda environments. Created sim-env directory
    
    * untracked doxygen files
    
    * figured out simulator-env for now
    
    * removing unnecessary files
    
    * removed doxygen dir and updated import_test

--- test/import_test.py
+++ test/import_test.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python
-
 print("******* Python Import Test *******")
-from input_generation import input_generation
-print("input_generation/input_generation.py imported successfully!")
-from time_difference_calculations import phase_analysis
-print("time_difference_calculations/phase_analysis.py imported successfully!")
-from TDOA_position_calculation import optimization, nonlinear_least_squares, analytical_solution
-print("TDOA_position_calculation/optimization.py imported successfully!")
-print("TDOA_position_calculation/nonlinear_least_squares.py imported successfully!")
-print("TDOA_position_calculation/analytical_solution.py imported successfully!")
+from sim_utils import config_parser, components_base, input_generation
+print("sim_utils/config_parser.py imported successfully!")
+print("sim_utils/components_base.py imported successfully!")
+print("sim_utils/input_generation.py imported successfully!")
