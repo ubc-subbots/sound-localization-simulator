1db7cd113e899edc1040f8fea46bfce627f7161f
dvirhilu <dvirhilu16@gmail.com>
2020-11-12 04:25:18 -0800

    added documentation markdown files

--- src/sim_utils/components_base.py
+++ src/sim_utils/components_base.py
@@ -6,10 +6,10 @@ Author: Michael Ko
 
 database = {
     "InputGeneration"       : "sim_utils.input_generation",
-    "InputGenerationStage"  : "components.input.input_generation_stage",
+    "InputGenerationStage"  : "stages.input.input_generation_stage",
     "IdealADC"              : "components.sampling.ideal_adc",
-    "IdealADCStage"         : "components.sampling.ideal_adc_stage",
+    "IdealADCStage"         : "stages.sampling.ideal_adc_stage",
     "PhaseAnalysis"         : "components.tdoa.phase_analysis",
-    "PhaseAnalysisStage"    : "components.tdoa.phase_analysis_stage",
+    "PhaseAnalysisStage"    : "stages.tdoa.phase_analysis_stage",
     "NLSPositionCalc"       : "components.position_calc.nls_position_calc"
 }
