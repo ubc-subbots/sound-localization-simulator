0820dafe61a0046a0dab514c76a57eacb5d501c0
Kota Chang <i@dongju.org>
2021-08-02 04:10:20 +0900

    reflections component

--- src/components/sound_propagation/test.py
+++ src/components/sound_propagation/test.py
@@ -6,6 +6,8 @@ import arlpy.uwapm as pm
 import arlpy
 import arlpy.plot as plt
 import numpy as np
+import pandas as pd
+import math
 
 bottom_absorption = 0 # for max reflection
 bottom_density = 1600 # default value
@@ -43,9 +45,22 @@ env = pm.create_env2d(
 #pm.print_env(env)
 
 #pm.plot_env(env, width=900)
-rays = pm.compute_eigenrays(env)
+#rays = pm.compute_eigenrays(env)
 #pm.plot_rays(rays, env=env, width=900)
 
 arrivals = pm.compute_arrivals(env)
-print(arrivals.keys())
 #pm.plot_arrivals(arrivals, width=900)
+#print(arrivals.sort_values(by="time_of_arrival")[['time_of_arrival', 'angle_of_arrival', 'surface_bounces', 'bottom_bounces']])
+#print(arrivals.sort_values(by="time_of_arrival")[['time_of_arrival']])
+toa = arrivals['time_of_arrival'].array
+print(len(toa))
+impulse_response = np.zeros(100)
+dt = 0.0005
+for i in range(len(toa)):
+        impulse_response[math.floor(toa[i]/dt)] = toa[i]
+print(impulse_response)
+print(len(impulse_response))
+print(np.delete(impulse_response,13))
+print(len(np.delete(impulse_response,13)))
+#print(toa.array)
+#print(np_toa[0])
