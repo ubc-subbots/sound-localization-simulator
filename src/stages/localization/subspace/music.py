1ab7b3cf4bd717087a2ca5872931df020b85ee10
dvirhilu <dvirhilu16@gmail.com>
2021-11-24 21:09:14 -0800

    added 3D functionality to MUSIC algorithm

--- src/stages/localization/subspace/music.py
+++ src/stages/localization/subspace/music.py
@@ -1,6 +1,8 @@
+from sys import int_info
 import global_vars
 import numpy as np
-from sim_utils.common_types import CylindricalPosition, PolarPosition, cyl_to_cart, CONV_2_DEG
+from numpy.linalg.linalg import matrix_rank
+from stages.localization.localization_utils import plane_wave_prop_delay
 from numpy.fft import fft
 from numpy.linalg import eig
 from sim_utils.output_utils import initialize_logger
@@ -11,22 +13,41 @@ import matplotlib.pyplot as plt
 class MUSIC:
 
     def __init__(self, resolution, plane_wave_propagation = True, 
-                 use_depth_sensor=True, visualize_jmusic=False, 
-                 halfplane=True):
+                 use_depth_sensor=False, visualize_jmusic=False, 
+                 xy_halfplane=True, r_max=50, num_r_cells=100):
         # create logger object for this module
         self.logger = initialize_logger(__name__)
         
         self.plane_wave_propagation = plane_wave_propagation
+        # determine model for calculating delays
+        if self.plane_wave_propagation:
+            self.delay_func = plane_wave_prop_delay
+        else:
+            self.logger.error("Only plane wave propagation modelled at the moment.")
+        
         self.use_depth_sensor = use_depth_sensor
         self.visualize_jmusic = visualize_jmusic
-        self.halfplane = halfplane
+
+        # set search range
+        self.max_phi = np.pi if xy_halfplane else 2*np.pi
+        self.r_max = r_max
+        self.num_r_cells = num_r_cells
         
         # error checking
         if resolution > 2*np.pi or resolution < 0:
             self.logger.error("Invalid resolution: %f. "
                          "Resolution must be between 0 and 2pi" % resolution)
         elif resolution > np.pi/10:
-            self.logger.warning("Resolution is too coarse and could lead to poor results.")
+            self.logger.warning("Resolution in phi is too coarse and could lead to poor results.")
+
+        if num_r_cells < 10 and use_depth_sensor:
+            self.logger.warning("Resolution in r is too coarse and could lead to poor results")
+        elif num_r_cells > 500 and use_depth_sensor:
+            self.logger.warning("Resolution in r is too fine and could lead to large computation time")
+
+        if r_max < 1:
+            self.logger.error("Invalid radial bound: %f. "
+                         "Radial bound must be greater than 1 to satisfy far field assumption" % resolution)
 
         self.resolution = resolution
 
@@ -34,12 +55,6 @@ class MUSIC:
         level = self.logger.getEffectiveLevel()
         if level <= logging.DEBUG:
             plot_signals(*sim_signal, title="Input to ADC")        
-
-        # determine model for calculating delays
-        if self.plane_wave_propagation:
-            delay_func = plane_wave_prop_delay
-        else:
-            self.logger.error("Only plane wave propagation modelled at the moment.")
 
         # take FFT of each signal
         y_fft = np.array([
@@ -55,39 +70,68 @@ class MUSIC:
         # discard vector with largest eigenvalue
         b_vectors = [v[:,i] for i in range(len(v)) if i != np.argmax(w)]
 
-        # loop over parameters
+        # generate parameter values to iterate over
+        param2_vals = np.arange(0, self.max_phi, self.resolution)
         if self.use_depth_sensor:
-            max_phi = np.pi if self.halfplane else 2*np.pi
-            phi_vals = np.arange(start=0, stop=max_phi, step=self.resolution)
-            steering_vectors = [
-                generate_steering_vector(delay_func, phi)
-                for phi in phi_vals
-            ]
-            j_music = [
-                calculate_music_coefficient(b_vectors, steering_vector)
-                for steering_vector in steering_vectors
-            ]
-
-            if self.visualize_jmusic:
-                fig = plt.figure()
-                ax = fig.add_subplot(111, projection='polar')
-                ax.plot(phi_vals, j_music)
-                ax.scatter(global_vars.pinger_position.phi, max(j_music), 
-                    label="Actual DOA")
-                plt.xlabel("Polar Angle " + r'$\phi$' + " (degrees)")
-                plt.ylabel(r'$J_{MUSIC}$')
-                plt.title(r'$J_{MUSIC}$' + " Distribution in " + r'$\phi$' + 
-                    "With DOA=%f" %(global_vars.pinger_position.phi/np.pi) + 
-                    r'$\pi$')
-                plt.legend()
-                plt.show()
-
-            # Return the value of phi that maximizes J_MUSIC
-            index = j_music.index(max(j_music))
-            return phi_vals[index]
+            param1_vals = np.linspace(1, self.r_max, self.num_r_cells)
         else:
-            self.logger.error("MUSIC not implemented without depth sensor yet.")
+            param1_vals = np.arange(0, np.pi/2, self.resolution)
+            # shift to lower hemisphere if we are above pinger
+            if global_vars.pinger_position.z < 0:
+                param1_vals += np.pi/2
+        
+        max_j_music = 0
+        max_params = None
+        # parameter to j_music dictionary for visualization
+        j_music_dict = {}
+        for p1 in param1_vals:
+            # sub dictionary for visualization
+            j_music_sub_dict = {}
+            for p2 in param2_vals:
+                # get steering vector for the parameter set
+                steering_vector = generate_steering_vector(self.delay_func, (p1, p2), 
+                                                           self.use_depth_sensor)
+                
+                # find MUSIC coefficient for parameter set
+                j_music = calculate_music_coefficient(b_vectors, steering_vector)
+                j_music_sub_dict[p2] = j_music
 
+                # evaluate whether the maximizing parameters should change
+                if j_music > max_j_music:
+                    max_j_music = j_music
+                    max_params = (p1, p2)
+
+            # add sub dictionary to main dictionary    
+            j_music_dict[p1] = j_music_sub_dict
+
+        if self.visualize_jmusic:
+            num_plots = 6
+            vis_indices = (np.linspace(0,1,num_plots)*len(param1_vals)).astype(int)
+            vis_p1_vals = param1_vals[vis_indices]
+            p1_name = "r" if self.use_depth_sensor else r'$\theta$'
+
+            num_fig_rows = int(np.sqrt(num_plots))
+            num_fig_cols = int(np.ceil(num_plots/num_fig_rows))
+            fig, axs = plt.subplots(num_fig_rows, num_fig_cols, 
+                                    projection='polar', figsize=(10,10))
+            fig.suptitle(r'$J_{MUSIC}$'+ " Coefficient Visualization")
+            axs = axs.flatten()
+            
+            for p1, ax in zip(vis_p1_vals, axs):
+                phi = j_music_dict[p1].keys()
+                j_music = j_music_dict[p1].values()
+                ax.plot(phi, j_music)
+                ax.scatter(global_vars.pinger_position.phi, max(j_music), 
+                            label="Actual DOA")
+                ax.set_title(r'$J_{MUSIC}$' + " Distribution in " + r'$\phi$' + 
+                             "With %s=%.2f" %(p1_name, p1))
+                ax.legend()
+                
+            plt.show()
+
+        # Return the parameter values that maximizes J_MUSIC
+        return max_params
+    
     def write_frame(self, frame):
         pass
 
@@ -113,41 +157,17 @@ def get_autocorrelation_matrix(sig_array, is_fft=True):
 
         return r_yy
 
-def plane_wave_prop_delay(sensor_pos, phi, theta=None):
-    # convert to numpy array for dot product
-    x = np.array(sensor_pos)
-
-    if theta:
-        # negative signs indicate propagation is towards sensor
-        khat = -np.array([
-            np.sin(theta)*np.cos(phi),
-            np.sin(theta)*np.sin(phi),
-            np.cos(theta)
-        ])
-    else:
-        # negative sign indicates propagation is towards sensor
-        khat = -np.array([
-            np.cos(phi),
-            np.sin(phi),
-            global_vars.pinger_position.z
-        ])
-
-    return np.dot(khat, x)/global_vars.speed_of_sound
-
-def generate_steering_vector(delay_func, phi, theta=None):
+def generate_steering_vector(delay_func, params, use_depth_sensor):
     omega = 2*np.pi*global_vars.signal_frequency
-    
-    # need the hydrophone positions in cartesian coordinates
-    if type(global_vars.hydrophone_positions) == CylindricalPosition:
-        h_positions = cyl_to_cart(global_vars.hydrophone_positions)
-    else:
-        h_positions = global_vars.hydrophone_positions
 
     # compute delay value at each hydrophone
-    tau_vals = [delay_func(hpos, phi, theta) for hpos in h_positions]
+    tau = np.array([
+        delay_func(params, hpos, use_depth_sensor) 
+        for hpos in global_vars.hydrophone_positions
+    ])
 
     # generate steering vector array from delay values
-    return np.array([np.e**(-1j*omega*tau) for tau in tau_vals])
+    return np.e**(-1j*omega*tau)
 
 def calculate_music_coefficient(b_vectors, steering_vector):
     # vector inner products
